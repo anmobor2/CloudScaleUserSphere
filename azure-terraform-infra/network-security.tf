48b5165d11e29027dbb64121202f7655239e0d17
Antonio  Moreno Borras <tonibandal@hotmail.com>
2024-10-13 22:23:56 +0200

    names

--- azure-terraform-infra/network-security.tf
+++ azure-terraform-infra/network-security.tf
@@ -5,13 +5,13 @@ resource "azurerm_virtual_network" "vnet" {
   name                = "myapp-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = var.location
-  resource_group_name = data.terraform_remote_state.acr.outputs.resource_group_name
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
 # Subnets
 resource "azurerm_subnet" "app_subnet" {
   name                 = "app-subnet"
-  resource_group_name  = data.terraform_remote_state.acr.outputs.resource_group_name
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
   service_endpoints    = ["Microsoft.Sql", "Microsoft.KeyVault"]
@@ -19,7 +19,7 @@ resource "azurerm_subnet" "app_subnet" {
 
 resource "azurerm_subnet" "db_subnet" {
   name                 = "db-subnet"
-  resource_group_name  = data.terraform_remote_state.acr.outputs.resource_group_name
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
   service_endpoints    = ["Microsoft.Sql"]
@@ -29,7 +29,7 @@ resource "azurerm_subnet" "db_subnet" {
 resource "azurerm_network_security_group" "app_nsg" {
   name                = "app-nsg"
   location            = var.location
-  resource_group_name = data.terraform_remote_state.acr.outputs.resource_group_name
+  resource_group_name = azurerm_resource_group.rg.name
 
   security_rule {
     name                       = "AllowHTTPInbound"
@@ -64,7 +64,7 @@ resource "azurerm_subnet_network_security_group_association" "app_nsg_associatio
 
 resource "azurerm_postgresql_virtual_network_rule" "postgresql_vnet_rule" {
   name                = "postgresql-vnet-rule"
-  resource_group_name = data.terraform_remote_state.acr.outputs.resource_group_name
+  resource_group_name = azurerm_resource_group.rg.name
   server_name         = azurerm_postgresql_server.postgresql.name
   subnet_id           = azurerm_subnet.app_subnet.id
 }
@@ -78,7 +78,7 @@ resource "azurerm_app_service_virtual_network_swift_connection" "vnet_integratio
 # Autoscale settings for App Service
 resource "azurerm_monitor_autoscale_setting" "app_autoscale" {
   name                = "app-autoscale"
-  resource_group_name = data.terraform_remote_state.acr.outputs.resource_group_name
+  resource_group_name = azurerm_resource_group.rg.name
   location            = var.location
   target_resource_id  = azurerm_app_service_plan.appserviceplan.id
 
